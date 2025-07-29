ee2954bd1bd5414d5903bebf5120fdde071b8536
discoverbasecode <discoverbasecode@gmail.com>
2025-07-29 23:02:44 +0330

    add entities helper in framework domain

---
+++ Framework/Framework.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,32 @@
+namespace Framework.Domain.Entities;
+
+public abstract class BaseEntity : IBaseEntity
+{
+    public Guid Id { get; set; }
+
+    public string? CreatedIp { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public string? CreatedLocation { get; set; }
+
+    public string? UpdatedIp { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+    public string? UpdatedBy { get; set; }
+    public string? UpdatedLocation { get; set; }
+
+    public string? DeletedIp { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime? DeletedAt { get; set; }
+    public string? DeletedBy { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public string? BlockedIp { get; set; }
+    public bool IsBlocked { get; set; }
+    public DateTime? BlockedAt { get; set; }
+    public string? BlockedBy { get; set; }
+
+    public bool IsApproved { get; set; }
+    public string? ApprovedBy { get; set; }
+    public DateTime? ApprovedAt { get; set; }
+}
---
+++ Framework/Framework.Domain/Entities/SeoInfo.cs
@@ -0,0 +1,16 @@
+namespace Framework.Domain.Entities;
+
+public class SeoInfo : ISeoInfo
+{
+    public string? Slug { get; set; }
+    public string? MetaTitle { get; set; }
+    public string? MetaDescription { get; set; }
+    public string? MetaKeywords { get; set; }
+    public string? PageTitle { get; set; }
+    public string? CanonicalUrl { get; set; }
+    public string? OgTitle { get; set; }
+    public string? OgDescription { get; set; }
+    public string? OgImage { get; set; }
+    public bool NoIndex { get; set; } = false;
+    public bool NoFollow { get; set; } = false;
+}
