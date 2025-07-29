e199f3400c6c03d38089c0391f7e3cb9af0ac37e
discoverbasecode <discoverbasecode@gmail.com>
2025-07-29 23:08:43 +0330

    add Value Object Address phone Seo Data in framework domain

---
+++ Framework/Framework.Domain/Common/ValueObjects/SeoData.cs
@@ -0,0 +1,26 @@
+namespace Framework.Domain.Common.ValueObjects;
+
+public class SeoData : ValueObject
+{
+    private SeoData()
+    {
+    }
+
+    public static SeoData CreateEmpty()
+    {
+        return new SeoData();
+    }
+
+    public SeoData(string? metaKeyWords, string? metaDescription, string? metaTitle, string? canonical)
+    {
+        MetaKeyWords = metaKeyWords;
+        MetaDescription = metaDescription;
+        MetaTitle = metaTitle;
+        Canonical = canonical;
+    }
+
+    public string? MetaTitle { get; private set; }
+    public string? MetaDescription { get; private set; }
+    public string? MetaKeyWords { get; private set; }
+    public string? Canonical { get; private set; }
+}
