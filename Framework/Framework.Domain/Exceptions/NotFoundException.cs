a843f34b93bf76bfc9d822bdba561576420d6fa8
discoverbasecode <discoverbasecode@gmail.com>
2025-07-30 22:02:34 +0330

    add NotFoundException in framework domain

---
+++ Framework/Framework.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace Framework.Domain.Exceptions;
+
+public class NotFoundException(string message) : Exception(message)
+{
+    public static NotFoundException Create(string entityName, object key)
+        => new NotFoundException($"{entityName} با شناسه '{key}' یافت نشد.");
+}
