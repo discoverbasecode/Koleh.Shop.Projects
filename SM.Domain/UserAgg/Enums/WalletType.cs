5073c9e5e33cacd23c684d50d0840770bf131088
discoverbasecode <discoverbasecode@gmail.com>
2025-07-31 00:03:11 +0330

    Add WalletType enum and enhance Wallet class

---
+++ SM.Domain/UserAgg/Enums/WalletType.cs
@@ -0,0 +1,10 @@
+﻿namespace SM.Domain.UserAgg.Enums;
+public enum WalletType
+{
+    Cash = 0,
+    Gift = 1,
+    Loyalty = 2,
+    Settlement = 3,
+    Deposit = 4,
+    Withdrawal = 5,
+}
--- SM.Domain/UserAgg/Wallet.cs
+++ SM.Domain/UserAgg/Wallet.cs
@@ -1,7 +1,48 @@
 ﻿using Framework.Domain.Entities;
+using Framework.Domain.Exceptions;
+using SM.Domain.UserAgg.Enums;
 
 namespace SM.Domain.UserAgg;
 
 public class Wallet : BaseEntity
 {
+    #region  Wallet Properties and Constructor and static Methods
+
+    public Guid UserId { get; private set; }
+    public decimal Price { get; private set; }
+    public string Description { get; private set; }
+    public bool IsFinally { get; private set; }
+    public DateTime FinallyDate { get; private set; }
+    public WalletType WalletType { get; private set; }
+
+    public Wallet() { }
+    private Wallet(Guid userId, decimal price, string description, WalletType walletType)
+    {
+        if (price <= 500)
+        {
+            throw InvalidFieldException.Create("قیمت", "قیمت باید بیشتر از ۵۰۰ تومان باشد.");
+        }
+        UserId = userId;
+        Price = price;
+        Description = description;
+        WalletType = walletType;
+        IsFinally = false;
+        FinallyDate = DateTime.MinValue;
+    }
+
+    public void Finally(string refCode)
+    {
+        IsFinally = true;
+        FinallyDate = DateTime.UtcNow;
+        Description = $"کد پیگیری {refCode}";
+    }
+
+    public void Finally()
+    {
+        IsFinally = true;
+        FinallyDate = DateTime.UtcNow;
+    }
+
+    #endregion
+
 }
