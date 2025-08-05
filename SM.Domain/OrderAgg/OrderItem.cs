cedfe5262173b9da636b3e3346ee32ded5ab9308
discoverbasecode <discoverbasecode@gmail.com>
2025-08-05 21:45:01 +0330

    add Order Item and add methods gouards and properties in Domain

---
+++ SM.Domain/OrderAgg/OrderItem.cs
@@ -0,0 +1,63 @@
+﻿using Framework.Domain.Exceptions;
+
+namespace SM.Domain.OrderAgg;
+
+public class OrderItem
+{
+    #region  OrderItem Properties and Constructor and static Methods
+
+    public Guid OrderId { get; private set; }
+    public Guid InventoryId { get; private set; }
+    public int Count { get; private set; }
+    public decimal Price { get; private set; }
+    public decimal TotalPrice => Price * Count;
+
+    public OrderItem() { }
+    private OrderItem(Guid inventoryId, int count, decimal price)
+    {
+        CountGuard(count);
+        PriceGuard(price);
+
+        InventoryId = inventoryId;
+        Count = count;
+        Price = price;
+    }
+
+    public void ChangeCount(int newCount)
+    {
+        CountGuard(newCount);
+
+        Count = newCount;
+    }
+
+    public void SetPrice(decimal newPrice)
+    {
+        PriceGuard(newPrice);
+        Price = newPrice;
+    }
+
+
+
+    #endregion
+
+    #region Guards Methods
+
+    public void PriceGuard(decimal newPrice)
+    {
+        if (newPrice < 1)
+        {
+            throw InvalidFieldException.Create("مبلغ", "مبلغ کالا معتبر نیست");
+        }
+    }
+
+    public void CountGuard(int count)
+    {
+        if (count < 1)
+        {
+            throw InvalidFieldException.Create("تعداد", "تعداد کالا معتبر نیست");
+        }
+    }
+
+    #endregion
+
+}
