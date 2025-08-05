4cc8cdd5a06941283d4f436aedea992ec435f713
discoverbasecode <discoverbasecode@gmail.com>
2025-08-05 21:44:33 +0330

    add Shipping Method and inheritanse  by Value Object inDomain

---
+++ SM.Domain/OrderAgg/ValueObjects/ShippingMethod.cs
@@ -0,0 +1,15 @@
+﻿using Framework.Domain.Common;
+
+namespace SM.Domain.OrderAgg.ValueObjects;
+
+public class ShippingMethod : ValueObject
+{
+    public string ShippingType { get; private set; }
+    public decimal ShippingCost { get; private set; }
+
+    public ShippingMethod(string shippingType, decimal shippingCost)
+    {
+        ShippingType = shippingType;
+        ShippingCost = shippingCost;
+    }
+}
