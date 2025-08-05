3324d23e52132905ca1703f486a60aa1f0c80d5c
discoverbasecode <discoverbasecode@gmail.com>
2025-08-05 21:44:15 +0330

    add Oder Discount , inheritanse  by Value Object inDomain

---
+++ SM.Domain/OrderAgg/ValueObjects/OrderDiscount.cs
@@ -0,0 +1,16 @@
+﻿using Framework.Domain.Common;
+
+namespace SM.Domain.OrderAgg.ValueObjects;
+
+public class OrderDiscount : ValueObject
+{
+    public OrderDiscount() { }
+    private OrderDiscount(string discountTitle, int discountAmount)
+    {
+        DiscountTitle = discountTitle;
+        DiscountAmount = discountAmount;
+    }
+
+    public string DiscountTitle { get; private set; }
+    public int DiscountAmount { get; private set; }
+}
