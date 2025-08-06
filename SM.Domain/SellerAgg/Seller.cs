cfc4799cf7119a9c8750c95fe10374679b73ff5e
discoverbasecode <discoverbasecode@gmail.com>
2025-08-06 10:23:59 +0330

    add seller inventory and seller aggregate to domain and set navigation relation and setup Guards in to

---
+++ SM.Domain/SellerAgg/Seller.cs
@@ -0,0 +1,44 @@
+﻿using Framework.Domain.Common;
+using Framework.Domain.Entities;
+using Framework.Domain.Exceptions;
+using SM.Domain.SellerAgg.Enums;
+
+namespace SM.Domain.SellerAgg;
+
+public class Seller : AggregateRoot
+{
+    #region Seller Properties and Constructor and static Methods
+
+    public Guid UserId { get; private set; }
+    public string ShopName { get; private set; }
+    public string NationalCode { get; private set; }
+    public string Description { get; private set; }
+    public SellerStatus SellerStatus { get; private set; }
+    public List<SellerInventory> Inventories { get; private set; }
+
+    public void ChangeSellerStatus(SellerStatus status)
+    {
+        SellerStatus = status;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    public void Edit(string shopName, string nationalCode)
+    {
+        ShopName = shopName;
+        NationalCode = nationalCode;
+        UpdatedAt = DateTime.UtcNow;
+    }
+
+    #endregion
+
+
+    #region Guards Method
+
+    private void Guard(string shopName, string nationalCode)
+    {
+        if (shopName == null)
+            throw InvalidFieldException.Create("نام فروشگاه", DomainMessageTemplate.Required);
+    }
+
+    #endregion
+}
---
+++ SM.Domain/SellerAgg/SellerInventory.cs
@@ -0,0 +1,43 @@
+﻿using Framework.Domain.Common;
+using Framework.Domain.Exceptions;
+
+namespace SM.Domain.SellerAgg;
+
+public class SellerInventory : AggregateRoot
+{
+    #region Seller Inventory Properties and Constructor and static Methods
+
+    public Guid SellerId { get; internal set; }
+    public Guid ProductId { get; private set; }
+    public string ProductName { get; private set; }
+    public uint Count { get; private set; }
+    public decimal Price { get; private set; }
+
+    public SellerInventory(Guid productId, string productName, uint count, decimal price)
+    {
+        PriceGuard(price);
+        CountGuard(count);
+        ProductId = productId;
+        ProductName = productName;
+        Count = count;
+        Price = price;
+    }
+
+    #endregion
+
+    #region Guards Method
+
+    private void PriceGuard(decimal price)
+    {
+        if (price < 1)
+            throw InvalidFieldException.Create("مبلغ", $"نمیتواند کمتر از {price} باشد");
+    }
+
+    private void CountGuard(uint count)
+    {
+        if (count < 1)
+            throw InvalidFieldException.Create("تعداد", $"نمیتواند کمتر از {count} باشد");
+    }
+
+    #endregion
+}
