10061cd4da017dc4aa6906f88ff190234bb900ce
discoverbasecode <discoverbasecode@gmail.com>
2025-08-06 08:37:33 +0330

    add Role and Set RolePermission list this and add Guard Title in domain

---
+++ SM.Domain/RoleAgg/Role.cs
@@ -0,0 +1,55 @@
+﻿using Framework.Domain.Entities;
+using Framework.Domain.Exceptions;
+
+namespace SM.Domain.RoleAgg;
+
+public class Role : BaseEntity
+{
+    #region Role Properties and Constructor and static Methods
+
+    private Role()
+    {
+    }
+
+    public string Title { get; private set; }
+    public List<RolePermission> Permissions { get; private set; }
+
+    public Role(string title)
+    {
+        TitleGuard(title);
+        Title = title;
+        Permissions = new List<RolePermission>();
+    }
+
+    public Role(string title, List<RolePermission> permissions)
+    {
+        TitleGuard(title);
+        Title = title;
+        Permissions = permissions;
+    }
+
+    public void SetPermission(List<RolePermission> permissions)
+    {
+        Permissions = permissions;
+    }
+
+    public void Edit(string title)
+    {
+        TitleGuard(title);
+        Title = title;
+        UpdatedAt = DateTime.Now;
+    }
+
+    #endregion
+
+
+    #region Guard Methods
+
+    public void TitleGuard(string title)
+    {
+        if (Title == null)
+            throw InvalidFieldException.Create("عنوان", "خالی است");
+    }
+
+    #endregion
+}
