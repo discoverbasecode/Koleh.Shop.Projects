e0ae22668b35d86729e1bc8374a3807c4d6eac77
discoverbasecode <discoverbasecode@gmail.com>
2025-08-07 15:21:15 +0330

    add create category command  validation in applicatio layer

---
+++ SM.Application/Features/Category/Validations/CreateCategoryValidation.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Framework.Application.ValidationExtensions;
+using SM.Application.Contracts.Features.Category;
+
+namespace SM.Application.Features.Category.Validations;
+
+public class CreateCategoryValidation : AbstractValidator<CreateCategoryCommand>
+{
+    public CreateCategoryValidation()
+    {
+        
+        RuleFor(c => c.Title)
+            .NotEmpty()
+            .WithMessage(c =>
+                ValidationMessages.RequiredField(nameof(c.Title)));
+      
+        RuleFor(c => c.Slug)
+            .NotEmpty()
+            .WithMessage(c =>
+                ValidationMessages.RequiredField(nameof(c.Slug)));
+    }
+}
