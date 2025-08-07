c57e314610b5afc75501c01413dd34cee72ae94e
discoverbasecode <discoverbasecode@gmail.com>
2025-08-07 15:31:33 +0330

    completed feature Category Child command handler validation in contracts and application layer

---
+++ SM.Application.Contracts/Features/Category/AddCategoryChildCommand.cs
@@ -0,0 +1,11 @@
+﻿using Ardalis.Result;
+using Framework.Application.MediatR.Command.Interfaces;
+using Framework.Domain.Entities;
+
+namespace SM.Application.Contracts.Features.Category;
+
+public record AddCategoryChildCommand(
+    Guid ParentId,
+    string Title,
+    string Slug,
+    ISeoInfo SeoInfo) : IBaseCommand;
---
+++ SM.Application/Features/Category/AddCategoryChildCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using Ardalis.Result;
+using Framework.Application.MediatR.Command.Handlers;
+using SM.Application.Contracts.Features.Category;
+using SM.Domain.CategoryAgg.Repository;
+using SM.Domain.CategoryAgg.Service;
+
+namespace SM.Application.Features.Category;
+
+public class AddCategoryChildCommandHandler(
+    ICategoryRepository categoryRepository,
+    ICategoryDomainService categoryDomainService) :
+    IBaseCommandHandler<AddCategoryChildCommand>
+{
+    public async Task<Result> Handle(AddCategoryChildCommand request, CancellationToken cancellationToken)
+    {
+        var category = await categoryRepository.GetByIdAsync(request.ParentId, cancellationToken);
+        if (category == null)
+            return Result.NotFound();
+
+        category.AddChild(request.Title, request.Slug, request.SeoInfo, categoryDomainService);
+        await categoryRepository.SaveChangesAsync(cancellationToken);
+
+        return Result.Success();
+    }
+}
