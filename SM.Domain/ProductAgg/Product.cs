9b71381e665b3f35a9adb31b548f63e11fe461bd
discoverbasecode <discoverbasecode@gmail.com>
2025-08-07 08:05:37 +0330

    add properties Product and Seo Info and navigation by Product Image and product Specification

---
+++ SM.Domain/ProductAgg/Product.cs
@@ -0,0 +1,26 @@
+﻿using Framework.Domain.Common;
+using Framework.Domain.Entities;
+
+namespace SM.Domain.ProductAgg;
+
+public class Product : AggregateRoot
+{
+    public string Title { get; private set; }
+    public string ImageName { get; private set; }
+    public string Descrition { get; private set; }
+    public Guid CategoryId { get; private set; }
+    public Guid SubCategoryId { get; private set; }
+    public string SecondarySubCategoryId { get; private set; }
+    public ISeoInfo SeoInfo { get; private set; }
+    public List<ProductImage> Images { get; private set; }
+    public List<ProductSpecification> Specifications { get; private set; }
+    
+}
+
+public class ProductImage : BaseEntity
+{
+}
+
+public class ProductSpecification : BaseEntity
+{
+}
