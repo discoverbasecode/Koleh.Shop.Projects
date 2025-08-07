76bfceb5a9b7c1ea6cf606fd61f896456d5f2547
discoverbasecode <discoverbasecode@gmail.com>
2025-08-07 11:09:04 +0330

    add aggregate  Comment and Add properties , method Crud , Comment Status in Domian

--- Framework/Framework.Domain/Exceptions/DomainMessageTemplate.cs
+++ Framework/Framework.Domain/Exceptions/DomainMessageTemplate.cs
@@ -6,8 +6,8 @@ public abstract class DomainMessageTemplate
     public const string NotEmpty = " خالی است";
     public const string InvalidField = "نامعتبر است";
     public const string NotFound = "اطلاعات درخواستی یافت نشد";
-    public const string MaxLength = "تعداد کاراکترهای وارد شده بیشتر از حد مجاز است";
-    public const string MinLength = "تعداد کاراکترهای وارد شده کمتر از حد مجاز است";
+    public const string MaxLength = " وارد شده بیشتر از حد مجاز است";
+    public const string MinLength = "وارد شده کمتر از حد مجاز است";
     public const string InvalidEmail = "نامعتبر است";
     public const string InvalidFormat = "فرمت مقدار وارد شده نامعتبر است";
     public const string RangeError = "مقدار وارد شده خارج از محدوده مجاز است";
---
+++ SM.Domain/CommentAgg/Comment.cs
@@ -0,0 +1,53 @@
+﻿using Framework.Domain.Common;
+using Framework.Domain.Exceptions;
+
+namespace SM.Domain.CommentAgg;
+
+public class Comment : AggregateRoot
+{
+    public Guid UserId { get; private set; }
+    public Guid ProductId { get; private set; }
+    public string Text { get; private set; }
+    public string RejectedText { get; private set; }
+    public CommentStatus Status { get; private set; }
+
+    public Comment(Guid userId, Guid productId, string text, string rejectedText)
+    {
+        Guards(text);
+        UserId = userId;
+        ProductId = productId;
+        Text = text;
+        RejectedText = rejectedText;
+        Status = CommentStatus.Pending;
+    }
+
+    public void Edit(string text)
+    {
+        Guards(text);
+        Text = text;
+        UpdatedAt = DateTime.Now;
+    }
+
+    public void ChangeStatus(CommentStatus status)
+    {
+        Status = status;
+        UpdatedAt = DateTime.Now;
+    }
+
+    private void Guards(string text)
+    {
+        if (Text == text)
+            throw InvalidFieldException.Create("متن", DomainMessageTemplate.NotEmpty);
+        if (text.Length < 5)
+            throw InvalidFieldException.Create("متن", DomainMessageTemplate.MinLength);
+        if (text.Length < 1000)
+            throw InvalidFieldException.Create("متن", DomainMessageTemplate.MaxLength);
+    }
+}
+
+public enum CommentStatus
+{
+    Pending,
+    Accepted,
+    Rejected,
+}
