2afdb0e1a9e58aa1213124e18dcc79e59c3cf5a4
discoverbasecode <discoverbasecode@gmail.com>
2025-08-07 15:05:01 +0330

    add Create Category Command and handler in Application and Application contracts layer

---
+++ SM.Application.Contracts/Features/Category/CreateCategoryCommand.cs
@@ -0,0 +1,10 @@
+﻿using Ardalis.Result;
+using Framework.Application.MediatR.Command.Interfaces;
+using Framework.Domain.Entities;
+
+namespace SM.Application.Contracts.Features.Category;
+
+public record CreateCategoryCommand(
+    string Title,
+    string Slug,
+    ISeoInfo SeoInfo) : IBaseCommand, IBaseCommand<Result>;
---
+++ SM.Application/Features/Category/CreateCategoryCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using Ardalis.Result;
+using Framework.Application.MediatR.Command.Handlers;
+using SM.Application.Contracts.Features.Category;
+using SM.Domain.CategoryAgg.Repository;
+using SM.Domain.CategoryAgg.Service;
+
+namespace SM.Application.Features.Category;
+
+public class CreateCategoryCommandHandler(
+    ICategoryRepository categoryRepository,
+    ICategoryDomainService categoryDomainService) : IBaseCommandHandler<CreateCategoryCommand>
+{
+    public async Task<Result> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var category = new Domain.CategoryAgg.Category(
+            request.Title, request.Slug, request.SeoInfo, categoryDomainService);
+        await categoryRepository.AddAsync(category, cancellationToken);
+        await categoryRepository.SaveChangesAsync(cancellationToken);
+        return Result.Success();
+    }
+}
